0ee15f9e9a697ad43678c254c3e85b0922649a69
rulyabdulrasyid <rrulyabdul@gmail.com>
2023-03-28 22:32:04 +0700

    second Commit

--- src/App.js
+++ src/App.js
@@ -1,70 +1,104 @@
+import * as React from "react";
+import { useState } from "react";
 
-import * as React from 'react';
+// 1. import `ChakraProvider` component
+import { ChakraProvider } from "@chakra-ui/react";
+import { Box, Button, Flex, Text, VStack } from "@chakra-ui/react";
 
 function Board() {
-  const squares = Array(9).fill(null);
-  function selectSquare(square) {
+  const [squares, setSquares] = useState(Array(9).fill(null));
 
+  const nextValue = calculateNextValue(squares);
+  const winner = calculateWinner(squares);
+  const status = calculateStatus(winner, squares, nextValue);
+
+  function selectSquare(square) {
+    if (winner || squares[square]) {
+      return;
+    }
+    const squaresCopy = [...squares];
+    squaresCopy[square] = nextValue;
+    setSquares(squaresCopy);
   }
 
   function restart() {
+    setSquares(Array(9).fill(null));
   }
 
   function renderSquare(i) {
     return (
-      <button className="square" onClick={() => selectSquare(i)}>
+      <Button
+        fontSize={50}
+        fontWeight="bold"
+        margin={2}
+        w="100px"
+        h="100px"
+        variant="outline"
+        borderWidth="2px"
+        borderColor="black"
+        className="square"
+        onClick={() => selectSquare(i)}
+      >
         {squares[i]}
-      </button>
+      </Button>
     );
   }
 
   return (
-    <div>
-      <div >STATUS</div>
-      <div >
+    <VStack>
+      <Text fontSize={40} fontWeight="bold" pt={6}>
+        {status}
+      </Text>
+      <Flex>
         {renderSquare(0)}
         {renderSquare(1)}
         {renderSquare(2)}
-      </div>
-      <div >
+      </Flex>
+      <Flex>
         {renderSquare(3)}
         {renderSquare(4)}
         {renderSquare(5)}
-      </div>
-      <div >
+      </Flex>
+      <Flex>
         {renderSquare(6)}
         {renderSquare(7)}
         {renderSquare(8)}
-      </div>
-      <button onClick={restart}>
-        restart
-      </button>
-    </div>
+      </Flex>
+      <Button
+        w="335px"
+        h="50px"
+        borderColor={"black"}
+        borderWidth="2px"
+        onClick={restart}
+      >
+        RESTART
+      </Button>
+    </VStack>
   );
 }
 
 function Game() {
   return (
-    <div >
-      <div >
+    <Box fontSize={40}>
+      <Box>
         <Board />
-      </div>
-    </div>
+      </Box>
+    </Box>
   );
 }
 
 // eslint-disable-next-line no-unused-vars
 function calculateStatus(winner, squares, nextValue) {
   return winner
-    ? `Winner: ${winner}`
+    ? `WINNER : ${winner}`
     : squares.every(Boolean)
-      ? `Scratch: Cat's game`
-      : `Next player: ${nextValue}`;
+    ? `SCRATCH : Cat's game`
+    : `NEXT PLAYER : ${nextValue}`;
 }
 
 // eslint-disable-next-line no-unused-vars
 function calculateNextValue(squares) {
-  return squares.filter(Boolean).length % 2 === 0 ? 'X' : 'O';
+  return squares.filter(Boolean).length % 2 === 0 ? "X" : "O";
 }
 
 // eslint-disable-next-line no-unused-vars
@@ -89,7 +123,11 @@ function calculateWinner(squares) {
 }
 
 function App() {
-  return <Game />;
+  return (
+    <ChakraProvider>
+      <Game />;
+    </ChakraProvider>
+  );
 }
 
 export default App;
